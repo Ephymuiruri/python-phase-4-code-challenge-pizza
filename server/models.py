6cb6fd72a0bd201238085c40998b2ceddbf8685f
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-07-04 12:55:13 +0300

    defined relationships and validations

--- server/models.py
+++ server/models.py
@@ -17,13 +17,14 @@ class Restaurant(db.Model, SerializerMixin):
     __tablename__ = "restaurants"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    address = db.Column(db.String)
+    name = db.Column(db.String,nullable=False)
+    address = db.Column(db.String,nullable=False)
 
     # add relationship
+    restaurants_pizzas = db.relationship('RestaurantPizza', backref='restaurant')
 
     # add serialization rules
-
+    serialize_rules=('-restaurant_pizzas.restaurant',)
     def __repr__(self):
         return f"<Restaurant {self.name}>"
 
@@ -32,13 +33,15 @@ class Pizza(db.Model, SerializerMixin):
     __tablename__ = "pizzas"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    ingredients = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String, nullable=False)
+
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
 
     # add serialization rules
-
+    serialize_rules = ('-restaurant_pizzas.pizza',)
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
 
@@ -48,11 +51,21 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+
+    @validates('price')
+    def validate_price(self, key, value):
+        if not (1 <= value <= 30):
+            raise ValueError("Price must be between 1and 30")
+        return value
 
     # add relationships
+    #restaurant = db.relationship('Restaurant', backref='pizzas')
+    #pizza = db.relationship('Pizza', backref='restaurant_pizzas')
 
     # add serialization rules
-
+    serialize_rules=('-pizza.restaurant_pizzas','-restaurant.restaurant_pizzas',)
     # add validation
 
     def __repr__(self):
